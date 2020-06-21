9e31a911cf476ebe007b531e4057e54a392ca6e9
Joseph S Gansukh <52771685+joseph-gansukh@users.noreply.github.com>
2020-06-20 23:23:47 -0500

    Update taskpane.js

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -43,6 +43,7 @@ function createTable() {
                 ["1/10/2017", "Coho Vineyard", "Restaurant", "33"],
                 ["1/11/2017", "Bellows College", "Education", "350.1"],
                 ["1/15/2017", "Trey Research", "Other", "135"],
+                ["1/15/2017", "Best For You Organics Company", "Groceries", "97.88"],
                 ["1/15/2017", "Best For You Organics Company", "Groceries", "97.88"]
             ]);
 
